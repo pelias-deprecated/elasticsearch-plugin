efced9174631022ac15e476c3ecca0f12b82b7b4
Francisco Dans <fdansv@gmail.com>
2014-10-15 16:02:06 +0200

    adds ascii folding, proper synonyms

--- src/main/java/org/apache/lucene/analysis/PeliasAnalyzer.java
+++ src/main/java/org/apache/lucene/analysis/PeliasAnalyzer.java
@@ -2,10 +2,29 @@ package org.apache.lucene.analysis;
 
 import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.core.WhitespaceTokenizer;
+import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilter;
 import org.apache.lucene.analysis.miscellaneous.WordDelimiterFilter;
+import org.apache.lucene.analysis.pattern.PatternReplaceFilter;
+import org.apache.lucene.analysis.synonym.SynonymFilter;
+import org.apache.lucene.analysis.synonym.SynonymMap;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.util.CharacterUtils;
+import org.apache.lucene.util.CharsRef;
 import org.apache.lucene.util.Version;
+import org.elasticsearch.PeliasPlugin.Tools;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import sun.misc.IOUtils;
 
+import javax.swing.text.html.HTMLDocument;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 
 
 public class PeliasAnalyzer extends Analyzer {
@@ -15,21 +34,22 @@ public class PeliasAnalyzer extends Analyzer {
         this.version = version;
     }
 
-
     @Override
     protected TokenStreamComponents createComponents(String fieldName,
                                                      Reader reader) {
         Tokenizer source = new WhitespaceTokenizer(version, reader);
         TokenStream filter = new LowerCaseFilter(version, source);
-        //TODO AMPERSAND FILTER
+        filter = new ASCIIFoldingFilter(filter);
+        //filter = new PatternReplaceFilter(filter, Pattern.compile("[&]"), " and ", true);
+        try {
+            JSONObject mappings = getMappings();
+            SynonymMap.Builder builder = addTerms(mappings);
+            SynonymMap synonyms = builder.build();
+            filter = new SynonymFilter(filter, synonyms, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         filter = new WordDelimiterFilter(version, filter, 0, null);
-//        SynonymMap synonymMap = null;
-//        try {
-//            synonymMap = new SynonymMap(new FileInputStream("samples/fulltext/wn_s.pl"));
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        filter = new SynonymFilter(filter, );
 
         return new TokenStreamComponents(source, filter);
     }
